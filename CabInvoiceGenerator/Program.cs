dada49c20a92982d514ade4fcfa5cd8975fc78f1
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-30 19:56:01 +0530

    [Vedhashni] Updated the file to Cab Invoice Generator Program

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Program");
+        }
+    }
+}
